80faa5764cef9c9f81a3bfcb594fd27f3fcc9ac6
aryodwicaksono08 <aryodwicaksono08@gmail.com>
2025-11-14 20:34:55 +0700

    main

---
+++ LuasLingkaran.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Math;
+
+class LuasLingkaran {
+
+    public const phi = 3.14;
+    protected int $jari;
+    
+    public function __construct($isiJari = 1) {
+        $this->jari = $isiJari; 
+    }
+
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
+    }
+
+    public static function testing() {
+        echo "<br/>";
+        echo "ini dari static";
+    }
+
+    public function __destruct() {
+        echo "<br/>";
+        echo "test1";
+    }
+}
+
+?>
