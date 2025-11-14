2189e0d9ff3990ae16b4694f1448adb6f4df15ff
aryodwicaksono08 <aryodwicaksono08@gmail.com>
2025-11-14 20:40:40 +0700

    pertemuan6

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once('./LuasLingkaran.php'); 
+
+use App\Math\LuasLingkaran; 
+
+$lingkaran = new LuasLingkaran(10);
+
+$lingkaran->tampil('roda'); 
+
+LuasLingkaran::testing(); 
+
+?>
